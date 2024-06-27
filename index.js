d0de4cebba8d71350aed000cd6cad3da1eff16a0
Mbuthiasakara1 <mbuthiaryne@gmail.com>
2024-06-27 21:07:42 +0300

    completes lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = "carol";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+  return bestCustomer;
+}
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+function changeLeastFavoriteCustomer() {
+  try {
+    leastFavoriteCustomer = "not carol";
+  } catch (e) {
+    throw new Error("Assignment to constant variable.");
+  }
+}
